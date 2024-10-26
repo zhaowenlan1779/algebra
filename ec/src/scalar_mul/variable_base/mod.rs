db74d357b3bc7379dc3050eaf4d1ff5bb07a3875
Pengfei Zhu <zhupf321@gmail.com>
2024-10-26 20:59:17 +0800

    Improve make_digits

--- ec/src/scalar_mul/variable_base/mod.rs
+++ ec/src/scalar_mul/variable_base/mod.rs
@@ -102,18 +102,15 @@ fn msm_bigint_wnaf<V: VariableBaseMSM>(
 
     let num_bits = V::ScalarField::MODULUS_BIT_SIZE as usize;
     let digits_count = (num_bits + c - 1) / c;
-    let scalar_digits = scalars
-        .iter()
-        .flat_map(|s| make_digits(s, c, num_bits))
-        .collect::<Vec<_>>();
+    let scalar_digits = make_digits(scalars, c, num_bits);
     let zero = V::zero();
     let window_sums: Vec<_> = ark_std::cfg_into_iter!(0..digits_count)
         .map(|i| {
             let mut buckets = vec![zero; 1 << (c - 1)];
-            for (digits, base) in scalar_digits.chunks(digits_count).zip(bases) {
+            for (digit, base) in scalar_digits[i * size..(i + 1) * size].iter().zip(bases) {
                 use ark_std::cmp::Ordering;
                 // digits is the digits thing of the first scalar?
-                let scalar = digits[i];
+                let scalar = *digit;
                 match 0.cmp(&scalar) {
                     Ordering::Less => buckets[(scalar - 1) as usize] += base,
                     Ordering::Greater => buckets[(-scalar - 1) as usize] -= base,
@@ -131,21 +128,16 @@ fn msm_bigint_wnaf<V: VariableBaseMSM>(
         })
         .collect();
 
-    // We store the sum for the lowest window.
-    let lowest = *window_sums.first().unwrap();
+    // We store the sum for the highest window.
+    let mut total = *window_sums.last().unwrap();
+    for i in (0..(window_sums.len() - 1)).rev() {
+        for _ in 0..c {
+            total.double_in_place();
+        }
+        total += &window_sums[i];
+    }
 
-    // We're traversing windows from high to low.
-    lowest
-        + &window_sums[1..]
-            .iter()
-            .rev()
-            .fold(zero, |mut total, sum_i| {
-                total += sum_i;
-                for _ in 0..c {
-                    total.double_in_place();
-                }
-                total
-            })
+    total
 }
 
 /// Optimized implementation of multi-scalar multiplication.
@@ -229,60 +221,58 @@ fn msm_bigint<V: VariableBaseMSM>(
         .collect();
 
     // We store the sum for the lowest window.
-    let lowest = *window_sums.first().unwrap();
+    let mut total = *window_sums.last().unwrap();
+    for i in (0..(window_sums.len() - 1)).rev() {
+        for _ in 0..c {
+            total.double_in_place();
+        }
+        total += &window_sums[i];
+    }
 
-    // We're traversing windows from high to low.
-    lowest
-        + &window_sums[1..]
-            .iter()
-            .rev()
-            .fold(zero, |mut total, sum_i| {
-                total += sum_i;
-                for _ in 0..c {
-                    total.double_in_place();
-                }
-                total
-            })
+    total
 }
 
-// From: https://github.com/arkworks-rs/gemini/blob/main/src/kzg/msm/variable_base.rs#L20
-fn make_digits(a: &impl BigInteger, w: usize, num_bits: usize) -> Vec<i64> {
-    let scalar = a.as_ref();
+fn make_digits<BigInt : BigInteger>(a: &[BigInt], w: usize, num_bits: usize) -> Vec<i64> {
     let radix: u64 = 1 << w;
     let window_mask: u64 = radix - 1;
-
-    let mut carry = 0u64;
     let num_bits = if num_bits == 0 {
-        a.num_bits() as usize
+        a[0].num_bits() as usize
     } else {
         num_bits
     };
     let digits_count = (num_bits + w - 1) / w;
-    let mut digits = vec![0i64; digits_count];
-    for (i, digit) in digits.iter_mut().enumerate() {
-        // Construct a buffer of bits of the scalar, starting at `bit_offset`.
-        let bit_offset = i * w;
-        let u64_idx = bit_offset / 64;
-        let bit_idx = bit_offset % 64;
-        // Read the bits from the scalar
-        let bit_buf = if bit_idx < 64 - w || u64_idx == scalar.len() - 1 {
-            // This window's bits are contained in a single u64,
-            // or it's the last u64 anyway.
-            scalar[u64_idx] >> bit_idx
-        } else {
-            // Combine the current u64's bits with the bits from the next u64
-            (scalar[u64_idx] >> bit_idx) | (scalar[1 + u64_idx] << (64 - bit_idx))
-        };
 
-        // Read the actual coefficient value from the window
-        let coef = carry + (bit_buf & window_mask); // coef = [0, 2^r)
+    let mut digits = vec![0i64; digits_count * a.len()];
 
-        // Recenter coefficients from [0,2^w) to [-2^w/2, 2^w/2)
-        carry = (coef + radix / 2) >> w;
-        *digit = (coef as i64) - (carry << w) as i64;
+    for (j, scalar) in a.iter().enumerate() {
+        let scalar = scalar.as_ref();
+        let mut carry = 0u64;
+        
+        for i in 0..digits_count {
+            // Construct a buffer of bits of the scalar, starting at `bit_offset`.
+            let bit_offset = i * w;
+            let u64_idx = bit_offset / 64;
+            let bit_idx = bit_offset % 64;
+            // Read the bits from the scalar
+            let bit_buf = if bit_idx < 64 - w || u64_idx == scalar.len() - 1 {
+                // This window's bits are contained in a single u64,
+                // or it's the last u64 anyway.
+                scalar[u64_idx] >> bit_idx
+            } else {
+                // Combine the current u64's bits with the bits from the next u64
+                (scalar[u64_idx] >> bit_idx) | (scalar[1 + u64_idx] << (64 - bit_idx))
+            };
+
+            // Read the actual coefficient value from the window
+            let coef = carry + (bit_buf & window_mask); // coef = [0, 2^r)
+
+            // Recenter coefficients from [0,2^w) to [-2^w/2, 2^w/2)
+            carry = (coef + radix / 2) >> w;
+            digits[i * a.len() + j] = (coef as i64) - (carry << w) as i64;
+        }
+
+        digits[(digits_count - 1) * a.len() + j] += (carry << w) as i64;
     }
-
-    digits[digits_count - 1] += (carry << w) as i64;
 
     digits
 }
